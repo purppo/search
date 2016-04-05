5f6ea65b026c299f0b04df5ebbde441c47dffa2a
Tauxix <tauxix@purppo.com>
2016-04-05 17:45:19 +0900

    first commit

---
+++ search/google.php
@@ -0,0 +1,25 @@
+<?php
+
+class google extends Thread
+{
+    public $url = 'https://www.google.co.jp/search?q=';
+    public function __construct($query,$host)
+    {
+        $this->query = $query;
+        $this->host = $host;
+    }
+ 
+    public function run()
+    {
+        //echo 'phantomjs '.dirname(__FILE__).'/getHtml.js '.escapeshellarg($this->url.urlencode($this->query)).PHP_EOL;
+        $this->html = shell_exec('phantomjs '.dirname(__FILE__).'/getHtml.js '.escapeshellarg($this->url.urlencode($this->query)));
+        
+        /*
+        $this->worker->addData(
+        );
+        
+        $this->html = file_get_contents('http://google.fr?q='.$this->query);
+         */
+    }
+}
+?>
